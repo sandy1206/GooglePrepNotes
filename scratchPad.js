f303832813d50076c85628e54a4724b259d4936e
Sandip Arsude <arsudesandip4@gmail.com>
2024-07-29 18:15:40 +0530

    bubble sort

--- scratchPad.js
+++ scratchPad.js
@@ -1,44 +1,14 @@
-function dfs(root){
-	let stack=[];
-
-	stack[0]= root;
-	while (stack.length){
-		let curr = stack.pop();
-		let left = curr.left
-		let right = curr.right;
-		if(left){
-			stack.push(left)
-		}
-		if (right) {
-			stack.push(right)
+function bubbleSort(arr) {
+	for (let i = arr.length - 1; i >= 0; i--) {
+		let j = 1;
+		while (j <= i) {
+			if (arr[j - 1] > arr[j]) {
+				[arr[j - 1], arr[j]] = [arr[j], arr[j - 1]]
+			}
+			j++;
 		}
 	}
+	return arr;
 }
 
-const tree = {
-	value: 1,
-	left: {
-		value: 2,
-		left: {
-			value: 4,
-			left: null,
-			right: null
-		},
-		right: {
-			value: 5,
-			left: null,
-			right: null
-		}
-	},
-	right: {
-		value: 3,
-		left: null,
-		right: {
-			value: 6,
-			left: null,
-			right: null
-		}
-	}
-};
-
-dfs(tree)
+console.log(bubbleSort([3,2,1,3,4,5,]));
