818a65772976fa3dc904b7d77f15a03578c1cc70
Sandip Arsude <arsudesandip4@gmail.com>
2024-08-05 14:52:31 +0530

    added sorting

--- scratchPad.js
+++ scratchPad.js
@@ -1,41 +1,28 @@
-function mergeInterval(arr){
-	let output=[];
-	arr.sort((a,b)=> a[0]-b[0])
-	console.log(arr);
-	let left= arr[0][0];
-	let right= arr[0][1];
+function mergeSort(arr, start, end) {
+	if (arr.length <= 1) return arr;
 
-	for(let i=1;i<arr.length;i++){
-		if(arr[i][0]>right){
-			output.push([left,right]);
-			left = arr[i][0];
-			right = arr[i][1]
-		}else{
-			right = Math.max(arr[i][1], right);
-		}
-	}
-	output.push([left, right]);
-	return output;
+	let middle = Math.floor(arr.length / 2);
+
+	return merge(mergeSort(arr.slice(0, middle)), mergeSort(arr.slice(middle)))
 }
 
-console.log(mergeInterval([[1,2],[1,3],[5,8],[4,7]]))
+function merge(arr1, arr2) {
+	let result = [];
+	let leftIndex = 0;
+	let rightIndex = 0;
 
-const insert = function (intervals, newInterval) {
-	let left = intervals[0][0];
-	let right = intervals[0][1];
-	let output = []
-
-	for (let i = 1; i < intervals.length; i++) {
-		if (right >= newInterval[0]) {
-			right = Math.max(newInterval[1], right)
+	while (leftIndex < arr1.length && rightIndex < arr2.length) {
+		if (arr1[leftIndex] <= arr2[rightIndex]) {
+			result.push(arr1[leftIndex]);
+			leftIndex++;
 		} else {
-			output.push([left, right]);
-			left = intervals[i][0]
-			right = intervals[i][1]
+			result.push(arr1[rightIndex]);
+			rightIndex++;
 		}
 	}
-	output.push([left, right]);
-	return output;
-};
+	return result.concat(arr1.slice(leftIndex)).concat(arr2.slice(rightIndex))
 
-console.log(insert([[1, 3], [6, 9]],[2,5]))
+}
+
+
+console.log(mergeSort([2,1,4,5,6,]))
