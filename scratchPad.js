0406111cdad635789398f750acfac04dca9c8919
Sandip Arsude <arsudesandip4@gmail.com>
2024-08-05 16:39:27 +0530

    test

--- scratchPad.js
+++ scratchPad.js
@@ -1,28 +1,34 @@
-function mergeSort(arr, start, end) {
+function waveSort(arr) {
+let temp= mergeSort(arr);
+	for (let i = 0; i < temp.length - 1; i += 2) {
+		[temp[i], temp[i + 1]] = [temp[i + 1], temp[i]];
+	}
+
+	return temp;
+}
+
+function mergeSort(arr) {
 	if (arr.length <= 1) return arr;
-
-	let middle = Math.floor(arr.length / 2);
-
-	return merge(mergeSort(arr.slice(0, middle)), mergeSort(arr.slice(middle)))
+	let mid = Math.floor(arr.length / 2);
+	return merge(mergeSort(arr.slice(0, mid)), mergeSort(arr.slice(mid)))
 }
 
 function merge(arr1, arr2) {
-	let result = [];
 	let leftIndex = 0;
 	let rightIndex = 0;
+	let result = [];
 
-	while (leftIndex < arr1.length && rightIndex < arr2.length) {
-		if (arr1[leftIndex] <= arr2[rightIndex]) {
+	while (leftIndex<arr1.length && rightIndex<arr2.length) {
+		if (arr1[leftIndex] < arr2[rightIndex]) {
 			result.push(arr1[leftIndex]);
 			leftIndex++;
 		} else {
-			result.push(arr1[rightIndex]);
+			result.push(arr2[rightIndex]);
 			rightIndex++;
 		}
 	}
-	return result.concat(arr1.slice(leftIndex)).concat(arr2.slice(rightIndex))
-
+	return result.concat(arr1.slice(leftIndex)).concat(arr2.slice(rightIndex));
 }
 
-
-console.log(mergeSort([2,1,4,5,6,]))
+console.log(mergeSort([3,2,4,65,6,1,6,3446,45,7,4,33,24]))
+console.log(waveSort([3, 2, 4, 65, 6, 1, 6, 3446, 45, 7, 4, 33, 24]))
